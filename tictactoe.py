e38d15e1fd1b00e328b3da94fe8aa5729fc9be23
Aaron McMillin <aaron@finiteloopsoftware.com>
2014-02-05 12:14:20 -0500

    Smart Player implemented
    TurnIterator improved with look ahead
    Game supports toggle for debug messages
    SmartPlayer wins or blocks if possible
    Allow user to choose difficulty level
    detect draws (why did that take so long to find?)

--- tictactoe.py
+++ tictactoe.py
@@ -2,6 +2,8 @@
 
 import curses
 import random
+from collections import Iterator
+
 
 LEFT_DIAGONAL = ((0, 0), (1, 1), (2, 2))
 RIGHT_DIAGONAL = ((0, 2), (1, 1), (2, 0))
@@ -9,16 +11,38 @@ RIGHT_DIAGONAL = ((0, 2), (1, 1), (2, 0))
 class SpaceOccupied(Exception):
     pass
 
-def turn_alternator():
-    while 1:
-        yield 1
-        yield 10
+
+class TurnAlternator(Iterator):
+    """Generate values from a list, repeating forever.
+    With a one item lookahead
+    """
+    def __init__(self, values):
+        self.cache = values[0]
+        self.gen = self._generate(values)
+        # eat the first value
+        next(self.gen)
+
+    def _generate(self, values):
+        while 1:
+            for v in values:
+                yield v
+
+    def next(self):
+        v = self.cache
+        self.cache = next(self.gen)
+        return v
+
+    def peek(self):
+        return self.cache
+
 
 class Game(object):
-    def __init__(self, window):
+    def __init__(self, window, debug=False):
         self.window = window
-        self.turns = turn_alternator()
+        self.DEBUG = debug
+        self.turns = TurnAlternator([1, 10])
         self.current_turn = self.turns.next()
+        self.won = False
         self.rows = (
             [0, 0, 0],
             [0, 0, 0],
@@ -41,14 +65,26 @@ class Game(object):
 
         self.display_state()
 
-        # Check Lines
+        # Check Lines for win
         for values, spaces in self.lines:
             # Only the current player can win
             if sum(values) == (3 * self.current_turn):
+                self.won = True
                 return True
 
-        self.current_turn = self.turns.next()
-        return False
+        # Check for Draw
+        for row in self.rows:
+            if 0 in row:
+                # Available move exists
+                self.current_turn = self.turns.next()
+                return False
+
+        #Draw
+        return True
+
+    @property
+    def next_turn(self):
+        return self.turns.peek()
 
     @property
     def lines(self):
@@ -128,6 +164,10 @@ class Game(object):
     def draw_x(self, y, x):
         self.draw_chr(y, x, 'X')
 
+    def debug(self, msg):
+        if self.DEBUG:
+            self.key_prompt(msg)
+
     def key_prompt(self, prompt):
         self.show_message(prompt)
         ch = self.window.getkey()
@@ -166,12 +206,73 @@ class Player(object):
     def __init__(self, game):
         self.game = game
 
-
-class PerfectPlayer(Player):
     def play(self):
-        pass
+        raise NotImplemented
+
+
+class RandomPlayer(Player):
+    def play(self):
+        """Find random open location, and play there"""
+        # faster, less typing
+        game = self.game
+
+        # Don't need output, just a press to continue
+        game.display_state()
+
+        open_list = [(y, x) for y, row in enumerate(game.rows) for x, val in enumerate(row) if val == 0]
+        if not open_list:
+            self.display_state()
+            game.show_message('The board is full')
+            return
+        move = random.choice(open_list)
+        game.debug('Random Playing to: {}'.format(move))
+        return game.play(*move)
+
+
+class SmartPlayer(RandomPlayer):
+    def _play(self):
+        """Returns game over as True or False if a move was made or None
+        if no move could be found
+        """
+
+        # faster, less typing
+        game = self.game
+
         # Win
+        for values, spaces in game:
+            if sum(values) == (2 * game.current_turn):
+                # Find the zero and play there
+                move = spaces[values.index(0)]
+                game.debug('Win Playing to: {}'.format(move))
+                return game.play(*move)
+
         # Block wins
+        for values, spaces in game:
+            if sum(values) == (2 * game.next_turn):
+                # Find the zero and play there
+                move = spaces[values.index(0)]
+                game.debug('Block Playing to: {}'.format(move))
+                return game.play(*move)
+        # No Move made
+        return None
+
+    def play(self):
+        # faster, less typing
+        game = self.game
+
+        result = self._play()
+
+        if result is None:
+            game.debug('Fall back to random')
+            # Random
+            result = super(SmartPlayer, self).play()
+
+        return result
+
+
+class PerfectPlayer(SmartPlayer):
+    def _play(self):
+        pass
         # Fork
         # Block fork
         # Center
@@ -179,24 +280,30 @@ class PerfectPlayer(Player):
         # any corner
         # side
 
-
-class RandomPlayer(Player):
     def play(self):
-        """Find random open location, and play there"""
-        # Don't need output, just a press to continue
-        self.game.display_state()
-        self.game.key_prompt('Ready?')
+        result = super(PerfectPlayer, self)._play()
 
-        open_list = [(y, x) for y, row in enumerate(self.game.rows) for x, val in enumerate(row) if val == 0]
-        if not open_list:
-            self.display_state()
-            self.game.show_message('The board is full')
-            return
-        return self.game.play(*random.choice(open_list))
+        if result is not None:
+            #Move made, return it
+            return result
+
+        result = self._play()
 
 
 def main(stdscr):
-    game = Game(stdscr)
+    PLAYERS = [RandomPlayer, SmartPlayer, PerfectPlayer]
+    game = Game(stdscr, debug=True)
+    game.display_state()
+
+    level = None
+    while level is None:
+        level = game.key_prompt('What difficulty would you like (1: Easy, 2: Smart, 3: Imposible) ')
+        if level not in '123':
+            level = None
+
+    level = int(level) - 1
+
+    # Clear message
     game.display_state()
 
     user_first = None
@@ -205,6 +312,7 @@ def main(stdscr):
         if user_first not in 'yn':
             user_first = None
 
+    # Clear message
     game.display_state()
 
     user_first = user_first == 'y'
@@ -216,15 +324,22 @@ def main(stdscr):
         game.show_message("Ok, I'll go first")
         stdscr.getch()
 
-    cpu = RandomPlayer(game)
+
+    cpu = PLAYERS[level](game)
 
     for t in xrange(9):
         if cpu.play():
-            game.show_message("I win !!!!")
+            if game.won:
+                game.show_message("I win !!!!")
+            else:
+                game.show_message("Draw")
             break
 
         if game.get_interactive_move():
-            game.show_message("You win !!!!")
+            if game.won:
+                game.show_message("You win !!!!")
+            else:
+                game.show_message("Draw")
             break
 
     stdscr.getch()
